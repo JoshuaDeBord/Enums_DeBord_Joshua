7d33b1373c50293845d665990cc38bec8a3b2476
JoshuaDeBord <joshua.debord@gctcok.edu>
2023-09-07 15:17:05 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Enums_DeBord_Joshua
+{
+    internal class Program
+    {
+        enum Months
+        {
+            Janaury=1,
+            Febuary,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
+
+        static void Main(string[] args)
+        {
+            int myBirthMonth = (int)Months.April;
+            int myBirthDay = 6;
+            int myBirthYear = 2005;
+            string myBirthDate = $"My birthday is on {myBirthMonth}/{myBirthDay}/{myBirthYear}.";
+            
+            File.WriteAllText("myFile.txt", myBirthDate);
+            Console.WriteLine(File.ReadAllText("myFile.txt"));
+            File.AppendAllText("myFile.txt", " My name is Joshua DeBord");
+
+            File.Copy("myFile.txt", "newfile.txt");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Enums_DeBord_Joshua
+ 
